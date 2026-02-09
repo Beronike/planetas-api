28201d4fc84bed613abcac4fe7ad85b0032de3c4
Veronica Vicent <vervicfor@alu.edu.gva.es>
2026-02-09 14:19:53 +0000

    Añado un segundo planeta (Marte2)

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const planetas = [
   { id: 1, nombre: 'Mercurio', orden: 1, tipo: 'Rocoso' },
   { id: 2, nombre: 'Venus', orden: 2, tipo: 'Rocoso' },
   { id: 3, nombre: 'Tierra', orden: 3, tipo: 'Rocoso' },
-  { id: 4, nombre: 'Marte', orden: 4, tipo: 'Rocoso' } 
+  { id: 4, nombre: 'Marte', orden: 4, tipo: 'Rocoso' },
+  { id: 5, nombre: 'Marte2', orden: 5, tipo: 'Rocoso' } 
 ];
 
 app.get('/planetas', (req, res) => {
